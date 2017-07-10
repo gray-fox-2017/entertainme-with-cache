c28814058667ca11ee89bd64f73ded4ea48f91bc
stedyyulius <stedyyulius@gmail.com>
2017-07-10 16:14:37 +0700

    added source

--- micro-service/Controllers/entertaiment-controllers.js
+++ micro-service/Controllers/entertaiment-controllers.js
@@ -20,14 +20,20 @@ const getEntertaiment = async (req,res) =>{
   var data = {}
   client.get(data,(err,result)=>{
     if(result){
-      res.send(JSON.parse(result))
+      res.send({
+        result: JSON.parse(result),
+        source: 'redis'
+      })
     } else{
       const entertaiments = {
         movies: movies.data,
         tv: tv.data
       }
       client.setex(data,10,JSON.stringify(entertaiments))
-      res.send(entertaiments)
+      res.send({
+        result: entertaiments,
+        source: 'local'
+      })
     }
   })
 }
