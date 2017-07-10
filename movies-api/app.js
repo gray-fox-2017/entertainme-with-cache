f4d3d44482d89e35846aa224a4bbe87cbac1d818
ingelieur <ingelieur@gmail.com>
2017-07-10 10:55:13 +0700

    initialization

---
+++ movies-api/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/magic-movies')
+
+const app = express()
+
+app.set('port', process.env.PORT || 3001)
+
+app.use(cors())
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+const index = require('./routes/index')
+
+app.use('/', index)
+
+app.listen(app.get('port'), () => {
+  console.log('Magic movies are happening at http://localhost:3001')
+})
